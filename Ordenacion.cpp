ffe837c26a3a75e714d1f31fbff9dc645a5cebf2
Marcooss1 <michaelmarcosmp@gmail.com>
2026-09-17 15:52:14 -0500

    Avance del primer programa INSERCCÍON DERECHA

---
+++ Ordenacion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arreglo[20];
+    int n;
+
+    cout << "Ingrese la cantidad de elementos: ";
+    cin >> n;
+
+    cout << "Ingrese los " << n << " elementos:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arreglo[i];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arreglo[j] > arreglo[j + 1]) {
+                int aux = arreglo[j];
+                arreglo[j] = arreglo[j + 1];
+                arreglo[j + 1] = aux;
+            }
+        }
+    }
+
+    cout << "\nArreglo ordenado: ";
+    for (int i = 0; i < n; i++) {
+        cout << arreglo[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
