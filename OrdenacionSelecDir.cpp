d0924dba64a674fd25238792660e692e69fa62d0
Marcooss1 <michaelmarcosmp@gmail.com>
2026-09-18 09:10:40 -0500

    Avance por Seleccion directa

---
+++ OrdenacionSelecDir.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Ingrese la cantidad de elementos: " << endl;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cout << "El elemento " << i + 1 << " es: ";
+        cin >> arr[i];
+    }
+
+
+    for (int i = 1; i < n - 1; i++) {
+        int menor = arr[i];
+        int k = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < menor) {
+                menor = arr[j];
+                k = j;
+            }
+        }
+
+        arr[k] = arr[i];
+        arr[i] = menor;
+    }
+
+
