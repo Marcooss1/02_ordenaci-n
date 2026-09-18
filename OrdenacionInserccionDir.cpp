77ea1740f78917a798ba96d5ce06f1a69126f059
Marcooss1 <michaelmarcosmp@gmail.com>
2026-09-18 09:54:32 -0500

    Avance por Insercion directa

---
+++ OrdenacionInserccionDir.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Ingrese la cantidad de elementos: ";
+	cin>>n;
+
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cout<<"Elemento "<<i+1<<" : ";
+	cin>>arr[i];
+	}
+
+	for(int i=2;i<n;i++){
+		int aux=arr[i];
+		int k=i-1;
+	while( k>0 || aux<arr[k] ){
+		arr[k+1]=arr[k];
+		k=k-1;
+		
+	}
+	arr[k+1]=aux;
+	}
+cout<<"arreglo ordenado: "<<endl;
+for(int i=0;i<n;i++){
+	cout<<arr[i]<<" ";
+}
+
+return 0;
+}
